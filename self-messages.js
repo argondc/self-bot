53fc2977bf7efa8be702936a2666720347b1e9e6
argondc <90789517+argondc@users.noreply.github.com>
2024-03-26 12:40:49 -0300

    refactor: compatibiliza para a versao v13

--- self-messages.js
+++ self-messages.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const { Client } = require('discord.js');
-const client = new Client({});
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client();
 
 const closeSelfBot = () => {
     console.log('Não existem mensagens para serem deletadas. Encerrando self-bot!');
@@ -11,41 +11,41 @@ client.once('ready', async () => {
     console.log(`Usuário ${client.user.username} conectado!`);
 });
 
-client.on('message', async message => {
+client.on('messageCreate', async message => {
     const channel = message.channel;
-    console.log(message)
     if (channel.type !== 'voice' &&
         channel.type !== 'category' &&
         channel.type !== 'news' &&
         channel.type !== 'store' &&
         message.content.includes('!argon')) {
         try {
-            const messages = await channel.fetchMessages();
-            const deleteMessagesWithInterval = async () => {
-                const queueMessages    = messages.array();
-                const delayInterval    = Number(process.env.DELAY_QUEUE);
-                const quantityMessages = Number(process.env.QUANTITY_MESSAGES);
-                let   index            = 0;
+            channel.messages.fetch().then(async messages => {
+                const deleteMessagesWithInterval = async () => {
+                    const queueMessages    = Array.from(messages.values());
+                    const delayInterval    = Number(process.env.DELAY_QUEUE);
+                    const quantityMessages = Number(process.env.QUANTITY_MESSAGES);
+                    let   index            = 0;
 
-                const deleteNextQueue = async () => {
-                    const chunk = queueMessages.slice(index, index + quantityMessages);
-                    index += quantityMessages;
-                    for (const msg of chunk) {
-                        try {
-                            await msg.delete();
-                            console.log(`Mensagem deletada! Conteúdo: ${msg.content}`);
-                        } catch (error) {
-                            console.log(`Problema ao deletar a mensagem, erro: ${error}`);
+                    const deleteNextQueue = async () => {
+                        const chunk = queueMessages.slice(index, index + quantityMessages);
+                        index += quantityMessages;
+                        for (const msg of chunk) {
+                            try {
+                                await msg.delete();
+                                console.log(`Mensagem deletada! Conteúdo: ${msg.content}`);
+                            } catch (error) {
+                                console.log(`Problema ao deletar a mensagem, erro: ${error}`);
+                            }
                         }
-                    }
 
-                    index < queueMessages.length ? setTimeout(deleteNextQueue, delayInterval) : closeSelfBot();
+                        index < queueMessages.length ? setTimeout(deleteNextQueue, delayInterval) : closeSelfBot();
+                    };
+
+                    await deleteNextQueue();
                 };
 
-                await deleteNextQueue();
-            };
-
-            await deleteMessagesWithInterval();
+                await deleteMessagesWithInterval();
+            });
         } catch (e) {
             console.log(`Problema ao encontrar as mensagens da dm, erro: ${e}`);
         }
